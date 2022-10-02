7a8ac3eb5de728c694c89a4b22ef6ed7347d141c
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-10-02 21:44:09 +0500

    added desicion for task 2

--- task_22.py
+++ task_22.py
@@ -2,3 +2,23 @@
 # произведений чисел от 1 до N.
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+
+number = input('enter the number > 0: ')
+while not (number.isdigit() and int(number) != 0):
+    number = input('enter the number > 0: ')
+number = int(number)
+
+product = 1
+text = '1'
+comment = [1,]
+product_set = []
+
+for i in range(1, number + 1):
+    product *= i
+    product_set.append(product)
+    if i < number:
+        text = text + '*' + str(i + 1)
+        comment.append(text)
+t = tuple(comment) # чтоб как в примере - с круглыми скобками :)
+print(f'набор произведений чисел от 1 до N:\n{product_set} {t}')
