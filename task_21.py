c55c127b0d39ea413101355010aa2bbf17ab2aaf
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-10-02 16:37:19 +0500

    added desicion for task 1

--- task_21.py
+++ task_21.py
@@ -2,3 +2,14 @@
 # Пример:
 # - 6782 -> 23
 # - 0,56 -> 11
+
+number = (input('enter the number: '))
+summ_of_numbers = 0
+int_in_input = []
+for i in number:
+    if not i.isdigit():
+        continue
+    if i.isdigit():
+        int_in_input.append(i)
+        summ_of_numbers += int(i)
+print(f'Numbers in the entered string: {int_in_input}\nTheir sum = {summ_of_numbers}')
