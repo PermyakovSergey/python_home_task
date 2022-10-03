5c27dd709408ec05c556611da459064b581ee58e
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-10-03 15:18:28 +0500

    added solution for task 3

--- task_23.py
+++ task_23.py
@@ -3,3 +3,13 @@
 # Пример:
 # - Для n = 6: [2, 2, 2, 2, 2, 3] -> 13
 
+n = 6
+
+formula_list = []
+for i in range(1, n + 1):
+    formula_list.append(round((1 + 1/i)**i))
+
+sum_of_elements = 0
+for i in formula_list:
+    sum_of_elements += i
+print(f'Для n = {n}: {formula_list} -> {sum_of_elements}')
