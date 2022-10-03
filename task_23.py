72684689916ef9f798975c80ab66a9e24a39e4cf
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-10-03 17:20:50 +0500

    added solution for task 5 not recursion

--- task_23.py
+++ task_23.py
@@ -6,10 +6,19 @@
 n = 6
 
 formula_list = []
+sum_of_elements = 0
 for i in range(1, n + 1):
     formula_list.append(round((1 + 1/i)**i))
+    sum_of_elements += formula_list[i - 1]
 
-sum_of_elements = 0
-for i in formula_list:
-    sum_of_elements += i
 print(f'Для n = {n}: {formula_list} -> {sum_of_elements}')
+
+# Для словаря:
+
+formula_dict = {}
+sum_of_values = 0
+for i in range(1, n + 1):
+    formula_dict[i] = round((1 + 1/i)**i)
+    sum_of_values += formula_dict[i]
+
+print(f'Для n = {n} {formula_dict} -> {sum_of_values}')
