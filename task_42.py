e786df72a88d69a4da4e19fe2bf04b471d924560
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-10-10 19:55:08 +0500

    task 42

--- task_42.py
+++ task_42.py
@@ -1,2 +1,13 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
+number = 3276
+list_of_mults = []
+
+while number != 1:
+    for simple_mult in range(2, number + 1):
+        if number % simple_mult == 0:
+            list_of_mults.append(simple_mult)
+            break
+    number = number // simple_mult
+
+print(list_of_mults)
