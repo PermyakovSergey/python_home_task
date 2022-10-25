61a971d765f1a10acd0cada31340f424a0b0f21e
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-10-25 23:27:44 +0500

    seminar 6 h tasks2

--- task_65.py
+++ task_65.py
@@ -5,7 +5,7 @@ from random import randint
 
 size = 10
 start_num = randint(1, size)
-lost_num = randint(2, size - 1)
+lost_num = randint(1, size - 1)
 with open('file.txt', 'w') as data: data.write(' '.join([str(start_num + i) for i in range(size + 1) if i != lost_num]))
 with open('file.txt', 'r') as data: n_list = list(map(int, data.read().split()))
 print((n_list), [(n_list[i-1] + 1) for i in range(1, len(n_list)) if n_list[i] - 1 != n_list[i-1]])
